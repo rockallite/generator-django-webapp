5c023287bb2761d9a8fd28d9dc56677233efd0df
Rockallite Wulf <rockallite.wulf@gmail.com>
2014-04-10 09:14:49 +0800

    Delete duplicated "serve" task in Gruntfile.js

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -637,20 +637,6 @@ module.exports = function (grunt) {
         ]);
     });
 
-    grunt.registerTask('serve', function (target) {
-        if (target === 'dist') {
-            return grunt.task.run(['build', 'connect:dist:keepalive']);
-        }
-
-        grunt.task.run([
-            'clean:server',
-            'concurrent:server',
-            'autoprefixer',
-            'connect:livereload',
-            'watch'
-        ]);
-    });
-
     grunt.registerTask('server', function (target) {
         grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
         grunt.task.run([target ? ('serve:' + target) : 'serve']);
